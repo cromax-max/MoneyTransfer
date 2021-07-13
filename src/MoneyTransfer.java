3b0145131aad680b74b277e657d9c976a02c4def
Sergey <tel.cromax@gmail.com>
2021-07-13 17:44:46 +1100

    add MoneyTransfer

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,12 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+
+        int clientBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int totalClientBalance = clientBalance + transferAmount;
+
+        System.out.println(totalClientBalance);
+
+
+    }
+}
